cef0236ccaa614eb9a9674063f7abc5c8cc418d8
WafflesAreFriends <johnfmunar@yahoo.com>
2019-08-06 05:18:27 -0700

    Push

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "GLFW"
   kind "StaticLib"
   language "C"
   targetdir("bin/"..outputdir.."/%{prj.name}")
-  outputdir("bin-int/"..outputdir.."/%{prj.name}")
+  objdir("bin-int/"..outputdir.."/%{prj.name}")
   
   files
   {
